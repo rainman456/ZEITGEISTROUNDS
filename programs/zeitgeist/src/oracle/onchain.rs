6eb4ec0a598afc7342233d234db46960d7bc2fec
rainman456 <81997753+rainman456@users.noreply.github.com>
2025-11-30 02:51:01 +0000

    hs

--- programs/zeitgeist/src/oracle/onchain.rs
+++ programs/zeitgeist/src/oracle/onchain.rs
@@ -1,33 +1,3 @@
-// use anchor_lang::prelude::*;
-// use anchor_lang::solana_program::sysvar::slot_history;
-// use crate::errors::SocialRouletteError;
-
-// pub fn verify_onchain_data(
-//     data_source: &AccountInfo,
-//     verification_type: &str,
-//     target_value: i64,
-// ) -> Result<u8> {
-//     match verification_type {
-//         "block_time" => {
-//             // Get recent slot
-//             let clock = Clock::get()?;
-//             let slot = clock.slot;
-            
-//             // Compare with target
-//             let winning_outcome = if (slot as i64) >= target_value {
-//                 0
-//             } else {
-//                 1
-//             };
-            
-//             Ok(winning_outcome)
-//         }
-//         _ => Err(SocialRouletteError::UnsupportedVerification.into()),
-//     }
-// }
-
-
-
 use anchor_lang::prelude::*;
 use crate::errors::SocialRouletteError;
 
